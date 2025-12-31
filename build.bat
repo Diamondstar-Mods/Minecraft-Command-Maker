48ae2c0858733a19d570b0174052a1006e627be1
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-12-31 15:18:09 +0000

    update build.yml

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ powershell cp server/build/libs/* CompiledFiles/
 echo Building CMDMaker Client version...
 call ./gradlew :client:build
 powershell cp client/build/libs/* CompiledFiles/
-
+powershell cp ./client/build/libs/CMDMaker-Fabric-client-* "C:/Users/adam/AppData/Roaming/PrismLauncher/instances/CommandMaker Client TestingInstance/minecraft/mods/"
 echo Build complete! 
