fab9dae207904653f2f850f3b56eb39d663c038a
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2026-04-09 14:27:48 +0100

    support 1.14 to 1.19

---
+++ server-legacy/src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Legacy server mod entrypoint for Minecraft 1.8 - 1.13.2.
+    }
+}
