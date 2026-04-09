a31c2d67eb285cc1188c172add41fabae1eeb61b
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2026-04-09 14:00:21 +0100

    unfinised versions for 1.17x 1.18x and 1.19x

---
+++ server-1.17/src/main/java/com/example/ExampleModClient.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+import com.example.gui.AliasDeleteScreen;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExampleModClient implements ClientModInitializer {
+	private static Map<String, String> aliases = new HashMap<>();
+
+	public static void setAliases(Map<String, String> aliasMap) {
+		aliases = new HashMap<>(aliasMap);
+	}
+
+	public static Map<String, String> getAliases() {
+		return new HashMap<>(aliases);
+	}
+
+	@Override
+	public void onInitializeClient() {
+		// Client-side initialization goes here
+	}
+}
+
